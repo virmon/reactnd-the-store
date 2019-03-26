f15b8778a6bfe4cab39322ea92b6fcf50afebe6a
virmon <virmontumulak@gmail.com>
2019-03-26 23:08:58 +0800

    feat: add listener for changes

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ function createStore() {
     */
 
     let state
+    let listeners = []
 
     const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+    }
 
     return {
         getState
     }
 }
+
+const store = createStore()
+store.subscribe(() => {
+    console.log("The new state is: ", store.getState())
+})
+store.subscribe(() => {
+    console.log("The store changed")
+})
