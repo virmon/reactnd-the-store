0ba2a4d21b61c25c313a6517ace4a26ce93668dd
virmon <virmontumulak@gmail.com>
2019-04-05 19:51:56 +0800

    feat: add notifyer middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,12 @@
 import checker from './checker'
 import logger from './logger'
-// import notifyer from './notifyer'
+import notifyer from './notifyer'
 import thunk from 'redux-thunk'
 import { applyMiddleware } from 'redux'
 
 export default applyMiddleware(
     thunk,
     checker,
+    notifyer,
     logger
 )
